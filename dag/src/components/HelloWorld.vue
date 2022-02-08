69f3836d8bc5f132cf728cee715c3dc02f8a6a23
saman <saman85430@gmail.com>
2022-02-08 11:47:40 +0000

    init app

---
+++ dag/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>Digital Art Gallery</h1>
+    <p>
+      your first place to enjoy iraqi art from local iraqi artests.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
